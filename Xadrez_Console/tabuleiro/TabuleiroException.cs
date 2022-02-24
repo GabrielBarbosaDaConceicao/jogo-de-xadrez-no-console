0a45641cbd32f953a8f27592eabddcbd1e71e208
Gabriel Conceicao <gabriel.bc.dev@hotmail.com>
2022-02-24 14:11:09 -0300

    Project Add

---
+++ Xadrez_Console/tabuleiro/TabuleiroException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Xadrez_Console.tabuleiro
+{
+    class TabuleiroException : Exception
+    {
+        public TabuleiroException(string msg) : base(msg)
+        {
+
+        }
+    }
+}
